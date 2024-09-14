5d60161d14f1ab59a7952a81338d84ee2ee49ab5
Luiscvj <Luiscarlos.villanjurado@gmail.com>
2024-09-13 22:06:14 -0500

    feat: Header en construccion

--- TrendG/src/app/app.routes.ts
+++ TrendG/src/app/app.routes.ts
@@ -1,12 +1,13 @@
 import { Routes } from '@angular/router';
 import { User } from './Core/Models/User/user';
 import { LoginComponent } from './Pages/Login/login/login.component';
+import { HeaderComponent } from './Shared/Header/header/header.component';
 
 export const routes: Routes = [
     {
         path: '',
-        component:LoginComponent,
-        title: 'Login user'
+        component:HeaderComponent,
+
 
         
     }
